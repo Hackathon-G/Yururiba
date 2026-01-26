e196a480742c51718cfc81660c9b15ce35f6ce8b
たか@59期 <takashimizu0529@gmail.com>
2026-01-26 23:42:49 +0900

    feature/errorブランチからファイルを流用

--- SNSApp/app.py
+++ SNSApp/app.py
@@ -1,23 +1,56 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template, session, flash, abort, url_for, make_response
 
 # Flaskアプリケーションのインスタンスを作成
-
 app = Flask(__name__)
 
 # ルートURL('/')へのGETリクエストを処理する関数を定義
-
 @app.route('/')
+def base_view():
+    return render_template('auth/base.html')
 
-def hello_world():
+# url_forのテスト
+# ログインページの表示
+@app.route('/login', methods=['GET'])
+def login_view():
+    return render_template('auth/login.html')
 
-    return 'Hello, Flask!'
+# 新規登録ページの表示
+@app.route('/register', methods=['GET'])
+def register_view():
+    return render_template('auth/register.html')
+
+# タイムラインページの表示
+@app.route('/timeline', methods=['GET'])
+def timeline_view():
+    return render_template('post/timeline.html')
+
+# 趣味選択ページの表示
+@app.route('/syumi', methods=['GET'])
+def syumi_view():
+    return render_template('post/syumi.html')
+
+# ぽめテストページの表示
+@app.route('/pome', methods=['GET'])
+def pome_view():
+    return render_template('error/pome.html')
+
+# ぽめテストページの表示（JSON版）
+@app.route('/pomeJSON', methods=['GET'])
+def pomeJSON_view():
+    return render_template('error/pomeJSON.html')
+
+# 404エラー確認用
+@app.route('/test', methods=['GET'])
+def error_404_test_view():
+    abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error/404.html'), 404
 
 # Pythonファイルとして直接実行された場合にサーバーを起動
-
 if __name__ == '__main__':
-
     # host='0.0.0.0'で外部からのアクセスも可能に（デフォルトは'127.0.0.1'）
-
     # port=5000はデフォルトポート
-
     app.run(debug=True, host='0.0.0.0')
+
